0232151900432fe0683d35805039b5ed19d9396a
dwaddell12 <dwaddell12@gmail.com>
2016-09-21 15:44:08 -0500

    Committing from SourceTree

--- PhotoGallery/Controllers/HomeController.cs
+++ PhotoGallery/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace PhotoGallery.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            
             return View();
         }
     }
